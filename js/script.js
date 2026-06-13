5d68ac90b930fb95cb4eff1f687384c92bc39c80
Miguel Romero <miguel.insti2@gmail.com>
2026-06-13 16:03:40 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game; // added
 
   startButton.addEventListener("click", function () {
     startGame();
@@ -8,5 +9,67 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+    game = new Game(); // added
+
+    game.start(); // added
   }
+   // Add an event listener to the restart button
+  restartButton.addEventListener("click", function () {
+    // Call the restartGame function when the button is clicked
+    restartGame();
+  });
+
+  // The function that reloads the page to start a new game
+  function restartGame() {
+    location.reload();
+  }
+  // Function that handles keydown event
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    // Check if the pressed key is in the possibleKeystrokes array
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -3;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -3;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 3;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 3;
+          break;
+      }
+    }
+  } 
+  
+  function handleKeyup(event) {
+  const key = event.key;
+  
+  switch (key) {
+    case "ArrowLeft":
+    case "ArrowRight":
+      game.player.directionX = 0;
+      break;
+    case "ArrowUp":
+    case "ArrowDown":
+      game.player.directionY = 0;
+      break;
+  }
+} 
+  window.addEventListener("keyup", handleKeyup);
+  // Add the handleKeydown function as an event listener for the keydown event
+  window.addEventListener("keydown", handleKeydown);
 };
